c624aacb400db2bbd991923680cadf4199a7bcfb
tykim9106-bit <tykim9106@gmail.com>
2026-09-06 09:38:31 +0900

    Step 13: add_positional_encoding_to_embeddings

--- model.py
+++ model.py
@@ -119,8 +119,10 @@ def build_sinusoidal_positional_encoding(max_len, d_model):
 
     return pe
 
-# Step 13 - add_positional_encoding_to_embeddings (not yet solved)
-# TODO: implement
+# Step 13 - add_positional_encoding_to_embeddings
+def add_positional_encoding_to_embeddings(embedded_batch, positional_encoding):
+    L = embedded_batch.shape[1]
+    return embedded_batch + positional_encoding[:L]
 
 # Step 14 - build_padding_mask (not yet solved)
 # TODO: implement
