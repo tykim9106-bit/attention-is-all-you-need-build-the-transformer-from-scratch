3c22d6d51c1258208db88e5ce80c6af9691223b1
tykim9106-bit <tykim9106@gmail.com>
2026-09-09 23:44:33 +0900

    Step 17: compute_raw_attention_scores

--- model.py
+++ model.py
@@ -148,8 +148,12 @@ def combine_padding_and_causal_masks(padding_mask, causal_mask):
         
 	return padding_mask * causal_mask
 
-# Step 17 - compute_raw_attention_scores (not yet solved)
-# TODO: implement
+# Step 17 - compute_raw_attention_scores
+import torch
+
+def compute_raw_attention_scores(query, key):
+    """Compute raw attention scores Q @ K^T over the last two dimensions."""
+    return torch.matmul(query, key.transpose(-2, -1))
 
 # Step 18 - scale_attention_scores (not yet solved)
 # TODO: implement
